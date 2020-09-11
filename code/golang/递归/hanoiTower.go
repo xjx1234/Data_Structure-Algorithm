10f3cceb72c33c11a6c6636081bd4a3113694e50
xjx <xjx@disanbo.com>
2020-09-11 18:13:38 +0800

    递归代码和文档更新

--- code/golang/递归/hanoiTower.go
+++ code/golang/递归/hanoiTower.go
@@ -7,6 +7,32 @@
 
 package main
 
-func main() {
+import "fmt"
 
+func hanota(A []int, B []int, C []int) []int {
+	n := len(A)
+	hanio(n, &A, &B, &C)
+	return C
+}
+
+func hanio(n int, a *[]int, b *[]int, c *[]int) {
+	if n < 1 || n > 14 {
+		return
+	} else if n == 1 {
+		*c = append(*c, (*a)[len(*a)-1])
+		*a = (*a)[:len(*a)-1]
+	} else {
+		hanio(n-1, a, c, b)
+		*c = append(*c, (*a)[len(*a)-1])
+		*a = (*a)[:len(*a)-1]
+		hanio(n-1, b, a, c)
+	}
+}
+
+func main() {
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	b := []int{}
+	c := []int{}
+	c = hanota(a, b, c)
+	fmt.Printf("%+v", c)
 }
--- code/golang/递归/linkReverse.go
+++ code/golang/递归/linkReverse.go
@@ -7,6 +7,63 @@
 
 package main
 
-func main() {
+import "fmt"
 
+//构造链表节点结构体
+type Node struct {
+	next *Node
+	data interface{}
+}
+
+//反转核心函数
+func reverse(node *Node) *Node {
+	if node.next == nil {
+		return node
+	} else {
+		newHead := reverse(node.next)
+		node.next.next = node
+		node.next = nil
+		return newHead
+	}
+}
+
+func main() {
+	numbers := []int{1, 2, 3, 4, 5, 6, 7}
+	head := &Node{data: "head", next: nil}
+	current := head
+
+	// 构造链表
+	for i := 0; i < len(numbers); i++ {
+		next := &Node{
+			data: numbers[i],
+			next: nil,
+		}
+		current.next = next
+		current = current.next
+		current.next = nil
+	}
+
+	fmt.Printf("正常链表：")
+	p := head
+	for true {
+		if p == nil {
+			break
+		} else {
+			fmt.Printf("%d->", p.data)
+			p = p.next
+		}
+	}
+	fmt.Printf("nil\n")
+	fmt.Printf("反转链表：")
+	head = reverse(head)
+	p = head
+	for true {
+		if p == nil {
+			break
+		} else {
+			fmt.Printf("%d->", p.data)
+			p = p.next
+		}
+	}
+	fmt.Printf("nil\n")
 }
