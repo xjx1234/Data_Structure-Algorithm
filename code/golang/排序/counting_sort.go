cf5b21955a55b155a58eaa157d0e2224d3d84976
xjx <xjx@disanbo.com>
2020-09-25 17:48:37 +0800

    更新快速排序与计数排序文档以及代码

---
+++ code/golang/排序/counting_sort.go
@@ -0,0 +1,48 @@
+/**
+* @Author: XJX
+* @Description: 计数排序实现
+* @File: counting_sort.go
+* @Date: 2020/9/25 15:27
+ */
+
+package main
+
+func counting_sort(data []int) []int {
+	if len(data) <= 1 {
+		return data
+	}
+
+	//取最大值
+	max := data[0]
+	for _, v := range data {
+		if v > max {
+			max = v
+		}
+	}
+
+	countArr := make([]int, max+1)
+	sortedArr := make([]int, len(data))
+
+	//统计每个数字出现的次数
+	for _, v := range data {
+		countArr[v]++
+	}
+
+	//统计数字累加和
+	for i := 1; i <= max; i++ {
+		countArr[i] += countArr[i-1]
+	}
+
+	//每个元素找到其位置
+	for _, v := range data {
+		sortedArr[countArr[v]-1] = v
+		countArr[v]--
+	}
+	return sortedArr
+}
+
+func main() {
+	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 8, 11}
+	counting_sort(data)
+	//fmt.Println(d)
+}
--- code/golang/排序/quick_sort.go
+++ code/golang/排序/quick_sort.go
@@ -7,20 +7,30 @@
 
 package main
 
+import "fmt"
+
 /** 快排实现算法 */
 func quick_sort(data []int, begin, end int) {
 	if begin >= end {
 		return
 	}
-	pivot := data[begin] //取数组第一个值为比较数据
-	start := begin
-
-
-
-	//quick_sort(data)
-	//quick_sort(data)
+	pivot := data[begin] //选取第一个为对比值
+	pivot_key := begin
+	for i := begin + 1; i <= end; i++ {
+		if data[i] < pivot {
+			data[pivot_key] = data[i]
+			data[i] = data[pivot_key+1]
+			pivot_key++
+		}
+	}
+	data[pivot_key] = pivot
+	quick_sort(data, begin, pivot_key-1)
+	quick_sort(data, pivot_key+1, end)
 }
 
 func main() {
-
+	data := []int{39, 28, 55, 87, 66, 3, 17, 39}
+	fmt.Println("排序前:", data)
+	quick_sort(data, 0, len(data)-1)
+	fmt.Println("排序后:", data)
 }
