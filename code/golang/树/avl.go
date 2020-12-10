dfb3a944dee202d6f30976ff3f6cfe09eeded86f
xjx <xjx@disanbo.com>
2020-12-10 18:41:02 +0800

    平衡二叉树新增内容

--- code/golang/树/avl.go
+++ code/golang/树/avl.go
@@ -15,6 +15,29 @@ type AVLNode struct {
 }
 
 /**
+ * @Description: 查询相关值节点
+ * @receiver t avinode节点
+ * @param val 查找值
+ * @return *AVLNode 返回查询值
+ */
+func (t *AVLNode) Serach(val int) *AVLNode {
+	if t == nil {
+		return nil
+	}
+	node := &AVLNode{}
+	if t.Val == val {
+		return t
+	} else if t.Val > val {
+		node = t.Left.Serach(val)
+	} else {
+		node = t.Right.Serach(val)
+	}
+	return node
+}
+
+
+
+/**
  * @Description:  左旋转
  * @receiver t avinode节点
  * @return *AVLNode 返回旋转后的节点
@@ -34,7 +57,34 @@ func (t *AVLNode) leftRotate() *AVLNode {
  * @return *AVLNode 返回旋转后的节点
  */
 func (t *AVLNode) rightRotate() *AVLNode {
+	rootNode := t.Left
+	t.Left = rootNode.Right
+	rootNode.Right = t
+	t.Height = max(t.Left.getHeight(), t.Right.getHeight()) + 1
+	rootNode.Height = max(rootNode.Left.getHeight(), rootNode.Right.getHeight()) + 1
+	return rootNode
+}
 
+/**
+ * @Description:  先左旋再右旋
+ * @receiver t avinode节点
+ * @return *AVLNode 返回旋转后的节点
+ */
+func (t *AVLNode) LeftThenRightRotate() *AVLNode {
+	sonRootNode := t.Left.leftRotate()
+	t.Left = sonRootNode
+	return t.rightRotate()
+}
+
+/**
+ * @Description: 先右旋再左旋
+ * @receiver t avinode节点
+ * @return *AVLNode 返回旋转后的节点
+ */
+func (t *AVLNode) rightThenLeftRotate() *AVLNode {
+	sonRootNode := t.Right.rightRotate()
+	t.Right = sonRootNode
+	return t.leftRotate()
 }
 
 /**
@@ -61,6 +111,38 @@ func (t *AVLNode) getHeight() int {
 		return 0
 	}
 	return t.Height
+}
+
+/**
+ * @Description: 获取最小值
+ * @receiver t 接收AVLNODE节点
+ * @return int 返回最小值
+ */
+func (t *AVLNode) getMin() int {
+	if t == nil {
+		return -1
+	}
+	cur := t.Left
+	for cur != nil {
+		cur = cur.Left
+	}
+	return cur.Val
+}
+
+/**
+ * @Description: 递归获取最大值
+ * @receiver t 接收AVLNODE节点
+ * @return int 返回最大值
+ */
+func (t *AVLNode) getMax() int {
+	if t == nil {
+		return -1
+	}
+	if t.Right == nil{
+		return t.Val
+	}else{
+		return t.Right.getMax()
+	}
 }
 
 func main() {
