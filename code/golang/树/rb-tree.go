3ccf374add2bbb317029b80c30ffb246a6814e3c
xjx <xjx@disanbo.com>
2020-12-21 19:14:24 +0800

    增加了红黑树以及部分代码

--- code/golang/树/rb-tree.go
+++ code/golang/树/rb-tree.go
@@ -8,12 +8,98 @@
 package main
 
 /**
+ * @Description: 定义红黑树的颜色
+ * @return nc
+ */
+const (
+	Red   = 1 //红色
+	Black = 2 //黑色
+)
+
+/**
  * @Description:  定义红黑树节点
  */
 type RBNode struct {
-	Color               bool
-	Left, Right, Father *RBNode
-	Val                 int
+	Color               bool    //颜色
+	Left, Right, Father *RBNode //左右和父节点
+	Val                 int     //值
+}
+
+/**
+ * @Description: 定义红黑树结构
+ */
+type RBTree struct {
+	root *RBNode
+}
+
+/**
+ * @Description: 红黑树左旋
+ *
+ *          node              tr(r)
+ *		   /  \              /  \
+ *        tl   tr(r)  =>    node rr
+ *            /  \         /  \
+ *           rl   rr      tl   rl
+ *
+ * @receiver t 红黑树
+ * @param node 红黑树节点
+ * @return *RBNode 返回红黑树
+ */
+func (t *RBTree) leftRotate(node *RBNode) {
+	if node != nil {
+		rootNode := node.Right
+		rootNode.Father = node.Father
+		if node.Father != nil {
+			if node.Father.Left == node {
+				node.Father.Left = rootNode
+			} else {
+				node.Father.Right = rootNode
+			}
+		} else {
+			t.root = rootNode
+		}
+		node.Father = rootNode
+		node.Right = rootNode.Left
+		if node.Right != nil {
+			node.Right.Father = node
+		}
+		rootNode.Left = node
+	}
+}
+
+/**
+ * @Description: 红黑树右旋
+ *
+ *          node             tl(r)
+ *		   /  \              /  \
+ *      (r)tl  tr  =>       ll   node
+ *       /  \                   / \
+ *      ll   lr               lr   tr
+ *
+ * @receiver t 红黑树
+ * @param node 红黑树节点
+ * @return *RBNode 返回旋转后的树
+ */
+func (t *RBTree) rightRotate(node *RBNode) {
+	if node != nil {
+		rootNode := node.Left
+		rootNode.Father = node.Father
+		if node.Father != nil {
+			if node.Father.Left == node {
+				node.Father.Left = rootNode
+			} else {
+				node.Father.Right = rootNode
+			}
+		} else {
+			t.root = rootNode
+		}
+		node.Father = rootNode
+		node.Left = rootNode.Right
+		if node.Left != nil {
+			node.Left.Father = node
+		}
+		rootNode.Right = node
+	}
 }
 
 func main() {
